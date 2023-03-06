7e2f7d819e4bdf13475be7d1542c94f70b0e91e0
Deepankar <dip2leo@gmail.com>
2023-03-06 13:22:43 -0500

    added frontend + spring

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button, Radio} from "antd";
+import {useState} from "react";
+function App() {
+
+  return (
+    <div className="App">
+        <Button type={"danger"}>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getAllStudents(){
+        List<Student> students = Arrays.asList(
+                new Student(1L, "My Name", "sjd@.com", Gender.Female),
+                new Student(2L, "Deep", "deep@.com", Gender.Male)
+        );
+        return students;
+    }
+}
